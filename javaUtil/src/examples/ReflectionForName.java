b96e6f14323d13823356df89ef138f7822c8a37a
jumbo225 <Jumb0@@%>
2017-06-11 07:58:01 +0530

    Reflection Class

---
+++ javaUtil/src/examples/ReflectionForName.java
@@ -0,0 +1,24 @@
+package examples;
+
+class Simple{
+	public void me(int i)
+	{
+		System.out.println("Hai"+i);
+	}
+}
+
+public class ReflectionForName {
+	public static void main(String[] args) {
+		try{
+			Class c=Class.forName("examples.Simple");
+			System.out.println(c.getName());
+			examples.Simple s=(Simple)c.newInstance();
+			s.me(4);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}		
+	}
+
+}
+
